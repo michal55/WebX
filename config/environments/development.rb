ed4a8138112c408027233dc088c1bb2ab01a7374
michal55 <michal.kren94@gmail.com>
2016-10-12 10:22:33 +0200

    Add email confirmation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,17 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { host: 'http://localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      user_name:  'tim.16.fiit.1617@gmail.com',
+      password:   '12345678',
+      domain:     'localhost:3000',
+      address:    'smtp.gmail.com',
+      port:       '587',
+      authentication: 'plain'
+  }
+
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -10,7 +10,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'authentication@webx.com'
+  config.mailer_sender = 'tim.16.fiit.1617@gmail.com'
 
   # Configure the class responsible to send e-mails.
   config.mailer = 'Devise::Mailer'
