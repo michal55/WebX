c0974da1d246e4fefcab985f6d860ecef7651ff2
johnybx <janoo.brechtl@gmail.com>
2016-10-14 12:50:41 -0700

    Added and configured capistrano

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-
   devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }, path: '', path_names: { sign_up: :join, sign_in: :login, sign_out: :logout }, module: :devise
 
   resources :users, only: [:index, :show]
