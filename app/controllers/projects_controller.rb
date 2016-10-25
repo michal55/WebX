aa05e3919633fb3a6fb2e6cd95f135620f338cfd
rastislav krchnavy <rastislav.krchnavy@gmail.com>
2016-10-25 23:59:37 +0200

    Created projects crud

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,19 +1,23 @@
 class ProjectsController < ApplicationController
 
-  def index
 
+  def index
+    @projects = Project.where(user_id: current_user.id)
   end
 
   def create
-
+    @project_new = Project.new
+    @project_new.assign_attributes({name: params[:project][:name], user_id:  current_user.id })
+    @project_new.save!
+    redirect_to projects_path
   end
 
   def new
-
+    @project_new = Project.new
   end
 
   def edit
-
+    @project = Project.find(params[:id])
   end
 
   def show
@@ -21,11 +25,16 @@ class ProjectsController < ApplicationController
   end
 
   def update
-
+    @project = Project.find(params[:id])
+    @project.name = params[:project][:name]
+    @project.save!
+    redirect_to projects_path
   end
 
   def destroy
-
+    @project = Project.find(params[:id])
+    @project.destroy!
+    redirect_to projects_path
   end
 
 end
