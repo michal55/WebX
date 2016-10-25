f83122a260c0fc92bfb56febdd288333a22e7f11
michal55 <michal.kren94@gmail.com>
2016-10-25 19:25:00 +0200

    Add tests for profile

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -10,6 +10,8 @@ class Ability
         can(:show, User) {|resource| resource == user}
     end
 
+    can(:profile, User)
+
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
