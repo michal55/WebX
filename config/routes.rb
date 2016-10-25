91283a9b1a08c9490458b3dd3fb032d8470d42b6
michal55 <michal.kren94@gmail.com>
2016-10-25 12:19:45 +0200

    Add user profile view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }, path: '', path_names: { sign_up: :join, sign_in: :login, sign_out: :logout }, module: :devise
 
-  resources :users, only: [:index, :show, :update]
+  resources :users, only: [:index, :show, :update, :profile]
+  get 'users/:id/profile' => 'users#profile', as: :profile
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
