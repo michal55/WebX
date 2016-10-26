2dfe44e2036ad8be1f6428a3adc07284b390fb58
michal55 <michal.kren94@gmail.com>
2016-10-26 22:09:18 +0200

    Create test for updating name in profile settings

--- spec/view/profile_spec.rb
+++ spec/view/profile_spec.rb
@@ -11,8 +11,26 @@ describe 'check profile', :type => :feature do
     click_button I18n.t('auth.login.submit')
 
     visit profile_path(user)
-    expect(page).to have_link(I18n.t('users.settings'))
+    expect(page).to have_content(user.email)
 
+  end
+
+  it 'change user name' do
+    user = create(:user)
+    visit new_user_session_path
+
+    fill_in 'user[email]', with: user.email
+    fill_in 'user[password]', with: user.password
+    click_button I18n.t('auth.login.submit')
+
+    visit edit_user_registration_path(user)
+    fill_in 'name', with: 'new_name'
+    fill_in 'user_current_password', with: user.password
+    # puts page.body
+    click_button 'Update'
+
+    visit profile_path(user)
+    expect(page).to have_content 'new_name'
   end
 
 end
