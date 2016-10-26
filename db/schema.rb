cbf6aad20940e9e0c2b793f00552a85b989c7975
michal55 <michal.kren94@gmail.com>
2016-10-26 13:48:27 +0200

    Add name to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161013160110) do
+ActiveRecord::Schema.define(version: 20161026111838) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20161013160110) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string   "unconfirmed_email"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
--- spec/view/profile_spec.rb
+++ spec/view/profile_spec.rb
@@ -15,19 +15,4 @@ describe 'check profile', :type => :feature do
 
   end
 
-  it 'check other profile' do
-    user = create(:user)
-    admin = create(:other_user)
-    admin.role = 'admin'
-    admin.save
-
-    visit new_user_session_path
-    fill_in 'user[email]', with: admin.email
-    fill_in 'user[password]', with: admin.password
-    click_button I18n.t('auth.login.submit')
-
-    visit profile_path(user)
-    expect(page).to_not have_link(I18n.t('users.settings'))
-
-  end
 end
