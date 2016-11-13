5273f0c5603fc5ae6730818b743ded0cb27b21bc
michal55 <michal.kren94@gmail.com>
2016-11-14 00:55:24 +0100

    Add api routes for projects and scripts

--- app/api/api/data/general.rb
+++ app/api/api/data/general.rb
@@ -14,6 +14,22 @@ module API
         get :user do
           User.find(doorkeeper_token[:resource_owner_id])
         end
+
+        resource :user do
+          get :projects do
+            Project.where(user_id: doorkeeper_token[:resource_owner_id])
+          end
+
+          resource :project do
+            get '/:id/scripts' do
+              Script.where(project_id: params[:id])
+            end
+          end
+        end
+
+        get :projects do
+          Project.all
+        end
       end
     end
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Doorkeeper::Application.create('name' => 'WebX Extension', 'uid' => '19e161e3cf0f594fb87f3e4810e041c45530861c56883ce09b2ff2a54a99fe39', 'secret' => 'ee113671f1a334fb9fba9289ca9af6a325cc890c1582d84adf35c25cc19ced6d', 'redirect_uri' => 'https://odfjpnhadepggnnihnhangccdfjophjo.chromiumapp.org/', 'scopes'=> '')
+Doorkeeper::Application.create('name' => 'WebX Extension', 'uid' => '19e161e3cf0f594fb87f3e4810e041c45530861c56883ce09b2ff2a54a99fe39', 'secret' => 'ee113671f1a334fb9fba9289ca9af6a325cc890c1582d84adf35c25cc19ced6d', 'redirect_uri' => 'https://kkdachbmijclgbobkhiodnpnmajmfnog.chromiumapp.org/', 'scopes'=> '')
