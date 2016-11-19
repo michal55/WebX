2a99b70707526d3d89c19018b3b5ea8516ae8261
Tomas Juhaniak <juhaniak.tomas@gmail.com>
2016-11-19 02:03:31 +0100

    Add first real data to view

--- app/controllers/extraction_datum_controller.rb
+++ app/controllers/extraction_datum_controller.rb
@@ -1,5 +1,8 @@
 class ExtractionDatumController < ApplicationController
   def index
-
+  	@extraction = Extraction.find(1)
+  	@extraction_datum = ExtractionDatum.where(extraction_id: 1)
+  	@script = Script.find(1)
+  	@project = Project.find(1)
   end
 end
