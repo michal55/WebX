52a234620c777e315da3b007fac101dad85c9ae9
Tomas Juhaniak <juhaniak.tomas@gmail.com>
2016-11-19 14:31:25 +0100

    Change fixed ids to ids from url

--- app/controllers/extraction_datum_controller.rb
+++ app/controllers/extraction_datum_controller.rb
@@ -1,8 +1,8 @@
 class ExtractionDatumController < ApplicationController
   def index
-  	@extraction = Extraction.find(1)
-  	@extraction_datum = ExtractionDatum.where(extraction_id: 1)
-  	@script = Script.find(1)
-  	@project = Project.find(1)
+  	@extraction = Extraction.find(params[:extraction_id])
+  	@extraction_datum = ExtractionDatum.where(extraction_id: @extraction.id)
+  	@script = Script.find(params[:script_id])
+  	@project = Project.find(params[:project_id])
   end
 end
