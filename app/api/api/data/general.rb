97ccfa5f351ea3a5443e8892f973731c69b268b8
johnybx <janoo.brechtl@gmail.com>
2016-11-25 12:55:40 -0800

    added api for save and load xpath

--- app/api/api/data/general.rb
+++ app/api/api/data/general.rb
@@ -1,4 +1,6 @@
 require 'doorkeeper/grape/helpers'
+require 'json'
+
 
 module API
   module Data
@@ -29,7 +31,42 @@ module API
             get '/:id/data_schemas' do
               DataSchema.where(project_id: params[:id])
             end
-          end
+
+            put '/:id/scripts/:id_script' do
+             error!('401 Unauthorized', 401) unless Script.find_by(id: params[:id_script]).project_id == params[:id].to_i and Project.find_by(id: params[:id]).user_id == doorkeeper_token[:resource_owner_id]
+              script = Script.find_by(id: params[:id_script])
+              script.xpaths = request.body.read
+              script.save!
+            end
+
+            get '/:id/scripts/:id_script' do
+              puts params[:id_script].respond_to?(:to_i)
+              puts Script.find_by(id: 11).project_id == params[:id_script]
+              puts params[:id]
+              puts params[:id_script]
+              error!('401 Unauthorized', 401) unless Script.find_by(id: params[:id_script]).project_id == params[:id].to_i and Project.find_by(id: params[:id]).user_id == doorkeeper_token[:resource_owner_id]
+              xpaths = Script.find_by(id: params[:id_script]).xpaths
+              if xpaths == nil
+                xpaths = {"url" => "" , "data" => []}
+                DataSchema.where(project_id: params[:id]).each do |d| xpaths["data"].append("name" => d.name, "value" => "") end
+                xpaths.to_json
+              else
+                #xpaths = JSON.parse(xpaths)
+                names = DataSchema.where(project_id: params[:id]).pluck("name")
+                del_indx = []
+                xpaths["data"].each do |xpath|
+                  if names.include?(xpath["name"])
+                    names.delete_at(names.index(xpath["name"]))
+                  else
+                    del_indx.append(xpaths["data"].index(xpath))
+                  end
+                end
+                del_indx.each do |indx| xpaths["data"].delete_at(indx) end
+                names.each do |name| xpaths["data"].append("name" => name, "value" => "") end
+                xpaths.to_json
+              end
+            end
+          end           
         end
 
         get :projects do
