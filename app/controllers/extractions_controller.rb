d248223a06de72a420060ecc0a5afe023fa5b0c7
xlacekm <Mato.lacek@gmailcom>
2016-11-27 16:21:23 +0100

    Added basic view to script extractions

--- app/controllers/extractions_controller.rb
+++ app/controllers/extractions_controller.rb
@@ -1,5 +1,7 @@
 class ExtractionsController < ApplicationController
   def index
-
+    @script = Script.find(params[:script_id])
+    @extraction = Extraction.where(script_id: @script.id)
+    @project = Project.find(params[:project_id])
   end
 end
