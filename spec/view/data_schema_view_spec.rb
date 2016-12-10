9fec357f8a9b16315692538b4cc0c0fbd5fce122
michal55 <michal.kren94@gmail.com>
2016-12-10 04:47:01 +0100

    Fix views and tests

--- spec/view/data_schema_view_spec.rb
+++ spec/view/data_schema_view_spec.rb
@@ -18,15 +18,7 @@ describe 'DataSchemaView', :type => :feature do
     click_button 'Log in'
 
 
-    visit project_data_fields_path(project.id)
-    expect(page).to have_link(data_field.name, href: edit_project_data_field_path(project.id,data_field.id))
-
-    visit edit_project_data_field_path(project.id,data_field.id)
-    expect(page).to have_selector('input[value="' + data_field.name + '"]')
-    expect(page).to have_content(data_field.data_type)
-    expect(page).to have_link(I18n.t('buttons.delete'), href: project_data_field_path(project.id,data_field.id))
-    expect(page).to have_button(I18n.t('buttons.submit'))
-    expect(page).to have_link(I18n.t('buttons.back'), href: project_data_fields_path(project.id))
-
+    visit project_path(project.id)
+    expect(page).to have_link(data_schema.name, href: edit_project_data_schema_path(project.id,data_schema.id))
   end
 end
--- spec/view/frequency_spec.rb
+++ spec/view/frequency_spec.rb
@@ -17,12 +17,13 @@ describe 'CRUD frequencies', :type => :feature do
     click_button 'Log in'
 
     visit project_script_path(project.id, script.id)
-    expect(page).to have_link(href: edit_project_script_frequency_path(project.id,script.id, frequency.id))
+    expect(page).to have_content(frequency.interval)
   end
 
   #TODO: fix test
   # it 'should add frequency in tables' do
-  #   script = create(:script)
+  #   frequency = create(:frequency)
+  #   script = frequency.script
   #   project = script.project
   #   user = script.project.user
   #   user.confirm
@@ -36,36 +37,16 @@ describe 'CRUD frequencies', :type => :feature do
   #   visit project_script_path(project.id,script.id)
   #   expect(page).to have_button(I18n.t('frequencies.new_button'))
   #
-  #   fill_in 'frequency[interval]', with: "5"
+  #   fill_in 'frequency[interval]', with: '5'
+  #   page.select 'hour', from: 'frequency_period'
+  #   fill_in 'frequency[first_exec]', with: '10/12/2016 04:41:00'
   #   click_button I18n.t('frequencies.new_button')
   #   sleep(5)
   #   visit current_path
   #   puts page.body
   #
-  #   expect(page).to have_content("5")
+  #   expect(page).to have_content('5')
   # end
-
-  it 'should modify frequency in tables' do
-    frequency = create(:frequency)
-    script = frequency.script
-    project = script.project
-    user = script.project.user
-    user.confirm
-    expect(user.confirmed?).to eq true
-    visit new_user_session_path
-
-    fill_in 'user[email]', with: user.email
-    fill_in 'user[password]', with: user.password
-    click_button 'Log in'
-
-    visit edit_project_script_frequency_path(project.id, script.id, frequency.id)
-    expect(page).to have_link(I18n.t('buttons.delete'), href: project_script_frequency_path(project.id, script.id, frequency.id))
-    expect(page).to have_button(I18n.t('buttons.submit'))
-    expect(page).to have_link(I18n.t('buttons.back'))
-    fill_in 'frequency[interval]', with: '235'
-    click_button 'Save'
-    expect(page).to have_content '235'
-  end
 
   it 'should delete frequency in tables' do
     frequency = create(:frequency)
@@ -83,5 +64,6 @@ describe 'CRUD frequencies', :type => :feature do
     visit project_script_path(project.id,script.id)
     page.click_link(href: project_script_frequency_path(project.id, script.id, frequency.id))
     expect(page).not_to have_link(href: project_script_frequency_path(project.id, script.id, frequency.id))
+    expect(page).not_to have_content(frequency.interval)
   end
 end
