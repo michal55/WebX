bf0183cd1a8bd3e946595d356db8659dbb0ea4de
michal55 <michal.kren94@gmail.com>
2017-02-19 19:10:14 +0100

    Add debug logging

--- app/services/crawler.rb
+++ app/services/crawler.rb
@@ -2,7 +2,20 @@ module Crawler
   extend self
   def execute(script)
     script_json = script.xpaths
-    @doc = Nokogiri::HTML(open(script_json['url']))
+    log = {}
+    log['status'] = ""
+    log['url'] = script_json['url']
+    begin
+      @doc = Nokogiri::HTML(open(script_json['url']))
+    rescue Exception => e
+      log['status'] = e.to_s
+      puts log
+      exit
+    end
+
+    log['html'] = @doc.to_s
+    log['xpaths'] = []
+
     extraction = Extraction.new
     extraction.script_id = script.id
     extraction.save!
@@ -10,16 +23,28 @@ module Crawler
     script.save!
 
     script_json['data'].each do |x|
+      subarray = []
       extraction_datum = ExtractionDatum.new
       extraction_datum.extraction_id = extraction.id
       extraction_datum.field_name = x['name']
       extraction_datum.value = @doc.xpath assert_text x['value']
+
+      subarray.push(x['value'])
+      subarray.push(x['name'])
+      subarray.push(extraction_datum.value)
+
       extraction_datum.save!
+      log['xpaths'].push(subarray)
     end
 
     extraction.execution_time = script.last_run - extraction.created_at
     extraction.success = true
     extraction.save!
+
+    log['exec_time'] = extraction.execution_time
+    log['status'] = extraction.success
+
+    puts log.to_json
 
   end
 
