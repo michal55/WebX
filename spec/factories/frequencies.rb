0cffda1a8986c15764371d6029774beeb1c8755e
rastislav krchnavy <rastislav.krchnavy@gmail.com>
2017-02-21 15:44:17 +0100

    Last run test

--- spec/factories/frequencies.rb
+++ spec/factories/frequencies.rb
@@ -9,7 +9,7 @@ FactoryGirl.define do
   end
 
   factory :frequency_not_run_yet, class: Frequency do
-    interval 5
+    interval 10
     period 'minute'
     last_run nil
     first_exec DateTime.parse("1.1.2017 13:00")
@@ -20,9 +20,9 @@ FactoryGirl.define do
   end
 
   factory :frequency_run_in_past, class: Frequency do
-    interval 5
-    period 'minute'
-    last_run nil
+    interval 3
+    period 'hour'
+    last_run DateTime.parse("21.2.2017 15:00")
     first_exec DateTime.parse("1.1.2017 13:00")
     after(:create) do |frequency|
       frequency.script = FactoryGirl.create(:script_with_xpaths)
--- spec/services/resque_spec.rb
+++ spec/services/resque_spec.rb
@@ -19,17 +19,17 @@ describe 'Enqueing job into queue' do
 
       # First run
       frequency = Frequency.find(frequency_id)
-      expect(frequency.last_run).to eq(DateTime.parse("1.1.2017 13:00"))
+      expect(frequency.last_run).to eq(DateTime.parse("2017-01-01 13:00:00 UTC"))
 
       # Should not run here
-      t = Time.parse("2017-01-01 13:03:00 UTC")
+      t = Time.parse("2017-01-01 13:08:00 UTC")
       Timecop.travel(t)
 
       Resque.enqueue(CrawlerInitializer)
       sleep(5)
 
       frequency = Frequency.find(frequency_id)
-      expect(frequency.last_run).to eq(DateTime.parse("1.1.2017 13:00"))
+      expect(frequency.last_run).to eq(DateTime.parse("2017-01-01 13:00:00 UTC"))
 
       # Here should run second time
       t = Time.parse("2017-02-02 17:28:00 UTC")
@@ -39,12 +39,52 @@ describe 'Enqueing job into queue' do
       sleep(5)
 
       frequency = Frequency.find(frequency_id)
-      expect(frequency.last_run).to eq(DateTime.parse("2.2.2017 17:25"))
-      # expect(true).to eq(true)
+      expect(frequency.last_run).to eq(DateTime.parse("2017-02-02 17:25:00 UTC"))
 
       # Only 2 extraction should run
       e = Extraction.all
       expect(e.size).to eq(2)
+    end
+  end
+
+  it 'should crawl running frequency' do
+
+    frequency = create(:frequency_run_in_past)
+    frequency_id = frequency.id
+    t = Time.parse("2017-02-21 14:58:00 UTC")
+    Timecop.travel(t)
+    with_resque do
+      # Enqueue
+      Resque.enqueue(CrawlerInitializer)
+      sleep(5)
+
+      # Should not run here
+      frequency = Frequency.find(frequency_id)
+      expect(frequency.last_run).to eq(DateTime.parse("2017-02-21 15:00:00 UTC"))
+
+      # Should not run here
+      t = Time.parse("2017-02-21 17:45:00 UTC")
+      Timecop.travel(t)
+
+      Resque.enqueue(CrawlerInitializer)
+      sleep(5)
+
+      frequency = Frequency.find(frequency_id)
+      expect(frequency.last_run).to eq(DateTime.parse("2017-02-21 15:00:00 UTC"))
+
+      # Here should run first time
+      t = Time.parse("2017-02-21 18:06:00 UTC")
+      Timecop.travel(t)
+
+      Resque.enqueue(CrawlerInitializer)
+      sleep(5)
+
+      frequency = Frequency.find(frequency_id)
+      expect(frequency.last_run).to eq(DateTime.parse("2017-02-21 18:05:00 UTC"))
+
+      # Only 1 extraction should run
+      e = Extraction.all
+      expect(e.size).to eq(1)
     end
   end
 end
