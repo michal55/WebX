7425dd6c638e984e80383d1865b82edbae589fd1
Tomas Juhaniak <juhaniak.tomas@gmail.com>
2017-02-22 14:09:47 +0100

    Add one-step creations --partial commit

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,6 +2,7 @@ class ProjectsController < ApplicationController
   load_and_authorize_resource :except => [:create]
 #  skip_authorize_resource :only => [:new, :index, :create]
   def index
+    @project_new = Project.new
     @projects = Project.where(user_id: current_user.id)
   end
 
@@ -27,6 +28,9 @@ class ProjectsController < ApplicationController
     @data_field = DataField.where(project: @project)
     @data_field_new = DataField.new
     @data_field_new.project = @project
+
+    @script_new = Script.new
+    @script_new.project_id = params[:project_id]
 
     authorize! :read, @project
   end
