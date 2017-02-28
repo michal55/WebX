5eb0fb1dcb3ec68902cf31489cc274013f3d8f76
michal55 <michal.kren94@gmail.com>
2017-02-28 16:54:40 +0100

    Refactor logging service

--- app/services/crawling/crawler.rb
+++ app/services/crawling/crawler.rb
@@ -5,16 +5,16 @@ module Crawling
     def execute(script)
       script_json = script.xpaths
       extraction = Extraction.new
-      extraction.script_id = script.id
+      extraction.script = script
       extraction.save!
 
-      Logging::Logger.debug('Extraction created', extraction.id, 'extraction')
+      Logging::Logger.debug('Extraction created', extraction, script.mode)
 
       # exit when opening URL fails
       begin
         @doc = Nokogiri::HTML(open(script_json['url']))
       rescue Exception => e
-        Logging::Logger.error(e.to_s, extraction.id, 'extraction')
+        Logging::Logger.error(e.to_s, extraction)
         extraction.success = false
         extraction.save!
         exit
@@ -24,21 +24,22 @@ module Crawling
       script.save!
 
       script_json['data'].each do |x|
-        extraction_datum = ExtractionDatum.new
+        extraction_datum               = ExtractionDatum.new
         extraction_datum.extraction_id = extraction.id
-        extraction_datum.field_name = x['name']
-        extraction_datum.value = @doc.xpath x['value']
+        extraction_datum.field_name    = x['name']
+        extraction_datum.value         = @doc.xpath x['value']
         extraction_datum.save!
+        extraction_datum
 
-        Logging::Logger.debug("field: #{x['name']}, xpath: #{x['value']}, value: #{extraction_datum.value}", extraction.id, 'extraction')
+        Logging::Logger.debug("field: #{x['name']}, xpath: #{x['value']}, value: #{extraction_datum.value}", extraction)
       end
 
       extraction.execution_time = script.last_run - extraction.created_at
       extraction.success = true
       extraction.save!
 
-      Logging::Logger.debug("Execution time: #{extraction.execution_time}", extraction.id, 'extraction')
-
+      Logging::Logger.debug("Execution time: #{extraction.execution_time}", extraction)
     end
+
   end
 end
--- app/services/logging/logger.rb
+++ app/services/logging/logger.rb
@@ -1,31 +1,38 @@
 module Logging
   module Logger
     extend self
+    @debug_level = 1
+    @warning_level = 2
+    @error_level = 3
 
     #usage: Logging::Logger.debug("error message", extraction.id)
 
-    def writer severity, msg, id, type
+    def set_log_level level
+      @log_level = level
+    end
+
+    def write severity, msg, id, type
       json = {}
       json['severity'] = severity
       json['msg'] = msg
       json['resource_id'] = id
       json['resource_type'] = type
-      output json
+      store json
     end
 
-    def debug msg, id, type
-      writer 1, msg, id, type
+    def debug msg, resource, level=@debug_level
+      write 1, msg, resource.id, resource.class.name.downcase if level <= @debug_level
     end
 
-    def warning msg, id, type
-      writer 2, msg, id, type
+    def warning msg, resource, level=@warning_level
+      write 2, msg, resource.id, resource.class.name.downcase if level <= @warning_level
     end
 
-    def error msg, id, type
-      writer 3, msg, id, type
+    def error msg, resource, level=@error_level
+      write 3, msg, resource.id, resource.class.name.downcase if level <= @error_level
     end
 
-    def output log
+    def store log
       # Posts to elastic index
       Log.create(log)
     end
