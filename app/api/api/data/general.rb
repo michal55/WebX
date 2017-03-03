b7f85916b3320090addd5096c379621cc3c40ac3
f4rnham <f4rnham@users.noreply.github.com>
2017-03-03 18:01:59 +0100

    Data field API will return all attributes (we will most likely need them in future anyway)

--- app/api/api/data/general.rb
+++ app/api/api/data/general.rb
@@ -29,7 +29,7 @@ module API
 
             get '/:id/data_fields' do
               error!('401 Unauthorized', 401) unless Project.find_by(id: params[:id]).user_id == doorkeeper_token[:resource_owner_id]
-              DataField.where(project_id: params[:id]).pluck(:name)
+              DataField.where(project_id: params[:id])
             end
             params do
               requires :url, type: String
@@ -44,10 +44,8 @@ module API
 
             get '/:id/scripts/:id_script' do
               error!('401 Unauthorized', 401) unless Script.find_by(id: params[:id_script]).project_id == params[:id].to_i and Project.find_by(id: params[:id]).user_id == doorkeeper_token[:resource_owner_id]
-              xpaths = Script.find_by(id: params[:id_script]).xpaths
-              xpaths
+              Script.find_by(id: params[:id_script]).xpaths
             end
-
           end
         end
       end
