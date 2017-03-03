b909a4dbb5e4bfd919ab9fb5ea772cf221631eda
f4rnham <f4rnham@users.noreply.github.com>
2017-03-03 18:42:55 +0100

    Update API/script tests - one with save + load is enough

--- spec/api/data/general_spec.rb
+++ spec/api/data/general_spec.rb
@@ -14,51 +14,23 @@ describe 'API data/', type: :request do
       expect(response.body).to eq(user.to_json)
     end
   end
+
   context 'json_script' do
-    it 'return empty json from script' do
-      script = create(:script_api) 
-      project_id = script.project_id
-      script_id = script.id
-      create(:data_field_general, name: "Integer", data_type: DataField.data_types[:integer], project_id: project_id)
-      create(:data_field_general, name: "String", data_type: DataField.data_types[:string], project_id: project_id)
-      access_token = create(:doorkeeper_token, resource_owner_id: Project.find_by(id: project_id).user_id)
-      get "/api/data/user/project/#{project_id}/scripts/#{script_id}", format: :json, access_token: access_token.token    
-      expect(response.body).to eq("{\"url\":\"\",\"data\":[{\"name\":\"Integer\",\"value\":\"\"},{\"name\":\"String\",\"value\":\"\"}]}")
-    end
-    it 'return json saved in script.xpaths because it is correct according to data schema' do
-      script = create(:script_api) #Script.find_by(name: "Script_Factory_api")
-      script_id = script.id
-      project_id = script.project_id
-      create(:data_field_general, name: "Integer", data_type: DataField.data_types[:integer], project_id: project_id)
-      create(:data_field_general, name: "String", data_type: DataField.data_types[:string], project_id: project_id)
-      access_token = create(:doorkeeper_token, resource_owner_id: Project.find_by(id: project_id).user_id)
-      script.xpaths = JSON.parse("{\"url\":\"www.google.com\",\"data\":[{\"name\":\"Integer\",\"value\":\"12345\"},{\"name\":\"String\",\"value\":\"tralala\"}]}")
-      script.save!
-      get "/api/data/user/project/#{project_id}/scripts/#{script_id}", format: :json, access_token: access_token.token    
-      expect(response.body).to eq("{\"url\":\"www.google.com\",\"data\":[{\"name\":\"Integer\",\"value\":\"12345\"},{\"name\":\"String\",\"value\":\"tralala\"}]}")
-    end
-    it 'return repaired json according to data schema' do
-      script = create(:script_api) #Script.find_by(name: "Script_Factory_api")
-      script_id = script.id
-      project_id = script.project_id
-      create(:data_field_general, name: "Integer", data_type: DataField.data_types[:integer], project_id: project_id)
-      create(:data_field_general, name: "String", data_type: DataField.data_types[:string], project_id: project_id)
-      access_token = create(:doorkeeper_token, resource_owner_id: Project.find_by(id: project_id).user_id)
-      script.xpaths = JSON.parse("{\"url\":\"www.google.com\",\"data\":[{\"name\":\"Testujeme\",\"value\":\"SSSDSDASDA\"},{\"name\":\"String\",\"value\":\"tralala\"}]}")
-      script.save!
-      get "/api/data/user/project/#{project_id}/scripts/#{script_id}", format: :json, access_token: access_token.token    
-      expect(response.body).to eq("{\"url\":\"www.google.com\",\"data\":[{\"name\":\"String\",\"value\":\"tralala\"},{\"name\":\"Integer\",\"value\":\"\"}]}")
-    end
-    it 'saves json into script.xpaths' do
+    it 'put API saves json into script.xpaths and get API returns the saved value' do
       script = create(:script_api)
       project_id = script.project_id
       script_id = script.id
+      script_data = '{"url":"www.google.com","data":[{"name":"Integer","value":"12345"},{"name":"String","value":"tralala"}]}'
       access_token = create(:doorkeeper_token, resource_owner_id: Project.find_by(id: project_id).user_id)
-      put "/api/data/user/project/#{project_id}/scripts/#{script_id}", format: :json, access_token: access_token.token ,'ACCEPT' => "application/json", 'CONTENT_TYPE' => 'application/json',"url"=>"www.google.com", "data"=>[{"name"=>"Integer", "value"=>"12345"}, {"name"=>"String", "value"=>"tralala"}]#, body => "{\"url\":\"\",\"data\":[{\"name\":\"Integer\",\"value\":\"\"},{\"name\":\"String\",\"value\":\"\"}]}"
+
+      put "/api/data/user/project/#{project_id}/scripts/#{script_id}", format: :json, access_token: access_token.token ,'ACCEPT' => 'application/json',
+          'CONTENT_TYPE' => 'application/json', 'url' => 'www.google.com', 'data' => [{'name' => 'Integer', 'value' => '12345'}, {'name' => 'String', 'value'=>'tralala'}]
       expect(response.response_code).to eq(200)
       script.reload
-      expect(script.xpaths.to_json).to eq("{\"url\":\"www.google.com\",\"data\":[{\"name\":\"Integer\",\"value\":\"12345\"},{\"name\":\"String\",\"value\":\"tralala\"}]}")
-    end
+      expect(script.xpaths.to_json).to eq(script_data)
 
+      get "/api/data/user/project/#{project_id}/scripts/#{script_id}", format: :json, access_token: access_token.token
+      expect(response.body).to eq(script_data)
+    end
   end
 end
