fdddc678b394b31455e979eac9aac3073266f45d
ajva46 <andrejvaculciak@windowslive.com>
2017-03-07 21:30:26 +0100

    Add preparation of test for nested script

--- spec/services/crawler_spec.rb
+++ spec/services/crawler_spec.rb
@@ -25,4 +25,29 @@ describe 'Extracting data from rubygems.org' do
     script = Script.find(script.id)
     expect(script.last_run).to be > script.created_at
   end
+
+  it 'should extract field names in footer' do
+    script = create(:script)
+    json = {}
+    json['url'] = "https://rubygems.org/"
+    json['data'] = []
+    json['data'][0] = {}
+    json['data'][0]['name'] = "navbar"
+    json['data'][0]['value'] = "/html/body/header/div/div/nav/a[1]/text()"
+    json['data'][0]['post_processing'] = {}
+    json['data'][0]['post_processing']['type'] = "nested"
+    json['data'][0]['post_processing']['data'] = []
+    json['data'][0]['post_processing']['data'][0]['name'] = "footer"
+    json['data'][0]['post_processing']['data'][0]['value'] = "/html/body/footer/div[3]/div[1]/a/text()"
+    script.xpaths = json.to_json
+    script.mode = 1
+    script.save
+    Crawling::Crawler.execute(script)
+    extraction = Extraction.find_by(script_id: script.id)
+    expect(extraction.success).to eq true
+    expect(extraction.extraction_data[0].value).to eq "Gems"
+    # add check if "English" has been crowled
+    script = Script.find(script.id)
+    expect(script.last_run).to be > script.created_at
+  end
 end
