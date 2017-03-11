e202a80f2cea0b8727645ced415112b728089864
ajva46 <andrejvaculciak@windowslive.com>
2017-03-11 20:11:43 +0100

    Update test for nested script

--- spec/services/crawler_spec.rb
+++ spec/services/crawler_spec.rb
@@ -26,28 +26,33 @@ describe 'Extracting data from rubygems.org' do
     expect(script.last_run).to be > script.created_at
   end
 
-  it 'should extract field names in footer' do
+  it 'should extract gem name and its version + nested hash value' do
     script = create(:script)
     json = {}
-    json['url'] = "https://rubygems.org/"
+    json['url'] = "https://rubygems.org/gems"
     json['data'] = []
     json['data'][0] = {}
-    json['data'][0]['name'] = "navbar"
-    json['data'][0]['value'] = "/html/body/header/div/div/nav/a[1]/text()"
-    json['data'][0]['post_processing'] = {}
-    json['data'][0]['post_processing']['type'] = "nested"
-    json['data'][0]['post_processing']['data'] = []
-    json['data'][0]['post_processing']['data'][0] = {}
-    json['data'][0]['post_processing']['data'][0]['name'] = "footer"
-    json['data'][0]['post_processing']['data'][0]['value'] = "/html/body/footer/div[3]/div[1]/a/text()"
+    json['data'][0]['name'] = "gem_name"
+    json['data'][0]['xpath'] = "/html/body/main/div/a[1]/span/h2"
+    json['data'][1] = {}
+    json['data'][1]['name'] = "version"
+    json['data'][1]['value'] = "/html/body/main/div/a[1]/span/h2/span/text()"
+    json['data'][1]['postprocessing'] = []
+    json['data'][1]['postprocessing'][0] = {}
+    json['data'][1]['postprocessing'][0]['type'] = "nested"
+    json['data'][1]['postprocessing'][0]['data'] = []
+    json['data'][1]['postprocessing'][0]['data'][0] = {}
+    json['data'][1]['postprocessing'][0]['data'][0]['name'] = "hash"
+    json['data'][1]['postprocessing'][0]['data'][0]['xpath'] = "/html/body/main/div/div/div[1]/div[3]/div[2]/text()"
     script.xpaths = json.to_json
     script.log_level = 1
     script.save
     Crawling::Crawler.execute(script)
     extraction = Extraction.find_by(script_id: script.id)
     expect(extraction.success).to eq true
-    expect(extraction.instances.first.extraction_data[0].value).to eq "Gems"
-    # add check if "English" has been crowled
+    #expect(extraction.instances.first.extraction_data[0].value).to eq "a"
+    expect(extraction.instances.first.extraction_data[1].value).to eq "0.1.1"
+    #expect(extraction.instances.first.extraction_data[2].value).to eq "233dca77fe5df2ef831943993ae7448963f89027929468622c30767a4e8f2357"
     script = Script.find(script.id)
     expect(script.last_run).to be > script.created_at
   end
