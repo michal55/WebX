4def9d0740906f9735867de88bf36389d12ceea8
michal55 <michal.kren94@gmail.com>
2017-03-16 15:12:12 +0100

    Refactor postprocessing, add parent_id to instances

--- app/services/crawling/crawler.rb
+++ app/services/crawling/crawler.rb
@@ -5,18 +5,23 @@ module Crawling
     def execute(script)
       @logger = Logging::Logger.new(severity: script.log_level)
       @agent = Mechanize.new
+      @post = Postprocessing.new
       script_json = script.xpaths
       extraction = Extraction.create(script: script)
       @logger.debug('Extraction created', extraction)
 
       # exit when opening URL fails
       @doc = try_get_url(extraction, script_json['url'])
-      exit if @doc.nil?
+      return if @doc.nil?
 
       script.last_run = Time.now
       script.save!
 
+      parent_stack = []
+
       instance = Instance.create(extraction_id: extraction.id)
+      instance.parent_id = instance.id.save
+      parent_stack.push(instance.id)
       script_json['data'].each do |row|
         extraction_datum = ExtractionDatum.create(
             instance_id: instance.id, extraction_id: extraction.id,
@@ -24,16 +29,16 @@ module Crawling
         )
         @logger.debug(log_msg(extraction_datum, row), extraction)
 
-        if is_nested(row['postprocessing'])
-          product_urls = extract_href(@doc, row['xpath'])
-          puts product_urls.size
+        if @post.is_nested(row['postprocessing'])
+          product_urls = @post.extract_href(@doc, row['xpath'])
           product_urls.each do |url|
             nested_page = try_get_url(extraction, url)
-            exit if nested_page.nil?
+            next if nested_page.nil?
 
+            new_instance = Instance.create(extraction_id: extraction.id, parent_id: parent_stack[-1])
             row['postprocessing'][0]['data'].each do |nested_row|  #TODO [0] upravit/doplnit
               extraction_datum = ExtractionDatum.create(
-                  instance_id: instance.id, extraction_id: extraction.id,
+                  instance_id: new_instance.id, extraction_id: extraction.id,
                   field_name: nested_row['name'], value: extract_value(nested_page, nested_row)
               )
               @logger.debug(log_msg(extraction_datum, nested_row), extraction)
@@ -41,6 +46,7 @@ module Crawling
           end
         end
       end
+      parent_stack.pop
 
       extraction.execution_time = script.last_run - extraction.created_at
       extraction.success = true
@@ -66,38 +72,11 @@ module Crawling
 
     def extract_value(doc, row)
       #TODO: refactor postprocessing
-      return extract_href(doc, row['xpath']) if is_nested(row['postprocessing'])
-      value = extract_text(doc, row['xpath'])
-      return value.to_s.strip if strip_whitespace(row['postprocessing'])
+      return @post.extract_href(doc, row['xpath']) if @post.is_nested(row['postprocessing'])
+      value = @post.extract_text(doc, row['xpath'])
+      return value.to_s.strip if @post.strip_whitespace(row['postprocessing'])
       value
     end
-
-    def strip_whitespace(row)
-      row.is_a?(Array) and row.size > 0 and row[0]['type'] == "whitespace"
-    end
-
-    def is_nested(row)
-      #TODO: kontrola na array nemusi byt idealna, uvidime co do toho jsonu este pribudne
-      row.is_a?(Array) and row.size > 0 and row[0]['type'] == "nested"
-    end
-
-    def extract_text doc, xpath
-      if xpath[-7..-1].eql?("/text()")
-        doc.parser.xpath(xpath)
-      else
-        doc.parser.xpath "#{xpath}/text()"
-      end
-    end
-
-    def extract_href doc, xpath
-      result = []
-      doc.parser.xpath(xpath).each do |link|
-        result.push(link.attributes['href'].to_s)
-      end
-      puts result
-      result
-    end
-
 
     def test_url
       agent = Mechanize.new
@@ -105,7 +84,8 @@ module Crawling
       doc = agent.get("https://www.alza.sk/notebooky/podla-vyuzitia/hracie/18848814.htm")
 
       # '//*[@id="prices"]/tbody/tr[3]/td/div/div/div[1]/div[2]/span[2]'
-      xpath = '//*[contains(@class, "categoryPageTitle")]'
+      # xpath = '//*[contains(@class, "categoryPageTitle")]'
+      xpath = '//*[@id="boxes"]/div/div[2]/div[1]/a'
       puts doc.parser.xpath(xpath)
     end
   end
