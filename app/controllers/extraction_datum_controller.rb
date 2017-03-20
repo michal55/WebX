75e1086570a3e30ad4f3c2febd1439a55ac663db
rastislav krchnavy <rastislav.krchnavy@gmail.com>
2017-03-20 18:46:08 +0100

    table init

--- app/controllers/extraction_datum_controller.rb
+++ app/controllers/extraction_datum_controller.rb
@@ -1,8 +1,26 @@
 class ExtractionDatumController < ApplicationController
+  require 'set'
+
+
   def index
   	@extraction = Extraction.find(params[:extraction_id])
-  	@extraction_datum = ExtractionDatum.where(extraction_id: @extraction.id)
+    @instances = Instance.where(extraction_id: @extraction.id)
+  	@extraction_datum_arr = []
+    @instances.each do |inst|
+      @extraction_datum_arr << inst.extraction_data
+    end
   	@script = Script.find(params[:script_id])
   	@project = Project.find(params[:project_id])
+
+    fields_set = Set.new
+
+    @extraction_datum_arr.each do |inst_data|
+      inst_data.each do |item|
+        fields_set.add(item.field_name)
+      end
+    end
+
+    @fields_array = fields_set.to_a
+
   end
 end
--- app/helpers/extraction_datum_helper.rb
+++ app/helpers/extraction_datum_helper.rb
@@ -1,2 +1,11 @@
 module ExtractionDatumHelper
+  def make_array(instance, fields_array)
+    extaction_data = instance.extraction_data
+    array = Array.new(fields_array.size,'-')
+    extaction_data.each do |ext|
+      array[fields_array.index(ext.field_name)] = ext.value
+    end
+    array
+  end
+
 end
