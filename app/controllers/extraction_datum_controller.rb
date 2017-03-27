c69c64245224bfc66a4085150ea6056e2d26946f
rastislav krchnavy <rastislav.krchnavy@gmail.com>
2017-03-27 21:56:46 +0200

    get data from parents

--- app/controllers/extraction_datum_controller.rb
+++ app/controllers/extraction_datum_controller.rb
@@ -5,10 +5,22 @@ class ExtractionDatumController < ApplicationController
   	@extraction = Extraction.find(params[:extraction_id])
     @instances = Instance.where(extraction_id: @extraction.id).page params[:page]
   	@extraction_datum_arr = []
+    @childs = []
+
+    parents_array = get_parents(@instances)
+    @parents = Instance.where(id: parents_array)
+
+    @parents.each do |inst|
+      @extraction_datum_arr << inst.extraction_data
+    end
 
     @instances.each do |inst|
       @extraction_datum_arr << inst.extraction_data
+      unless parents_array.include?(inst.id)
+        @childs << inst
+      end
     end
+
 
     @script = Script.find(params[:script_id])
   	@project = Project.find(params[:project_id])
@@ -24,4 +36,19 @@ class ExtractionDatumController < ApplicationController
     @fields_array = fields_set.to_a
 
   end
+
+  private
+
+  def get_parents(instances)
+    parents_set = Set.new
+    instances.each do |i|
+      inst = i
+      while not parents_set.include?(inst.parent_id)
+        parents_set.add(inst.parent_id)
+        inst = Instance.find(inst.parent_id)
+      end
+    end
+    parents_set.to_a
+  end
+
 end
--- app/helpers/extraction_datum_helper.rb
+++ app/helpers/extraction_datum_helper.rb
@@ -1,11 +1,24 @@
 module ExtractionDatumHelper
   def make_array(instance, fields_array)
-    extaction_data = instance.extraction_data
-    array = Array.new(fields_array.size,'-')
-    extaction_data.each do |ext|
-      array[fields_array.index(ext.field_name)] = ext.value
+
+    extraction_data_arr = []
+    inst = instance
+    loop do
+      extraction_data_arr << inst.extraction_data
+      break if inst.id == inst.parent_id
+      inst = Instance.find(inst.parent_id)
     end
+
+    array = Array.new(fields_array.size,'-')
+
+    extraction_data_arr.each do |extraction_data|
+      extraction_data.each do |ext|
+        array[fields_array.index(ext.field_name)] = ext.value if array[fields_array.index(ext.field_name)] == '-'
+      end
+    end
+
     array
   end
+
 
 end
