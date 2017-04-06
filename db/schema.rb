5cccc2395acf43384abbf31f7460b3c9e4a1b15c
michal55 <michal.kren94@gmail.com>
2017-04-06 19:56:49 +0200

    API instructions fix

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170403082141) do
+ActiveRecord::Schema.define(version: 20170406114613) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,6 +69,7 @@ ActiveRecord::Schema.define(version: 20170403082141) do
     t.integer  "extraction_id"
     t.integer  "parent_id"
     t.datetime "deleted_at"
+    t.boolean  "is_leaf"
   end
 
   add_index "instances", ["deleted_at"], name: "index_instances_on_deleted_at", using: :btree
