d5e9400959ea9dd34eaaf2a8b36ef0631a1e3103
michal55 <michal.kren94@gmail.com>
2017-04-07 13:02:56 +0200

    Remove pagination from data to export

--- app/helpers/extraction_datum_helper.rb
+++ app/helpers/extraction_datum_helper.rb
@@ -8,4 +8,8 @@ module ExtractionDatumHelper
     array.to_csv(row_sep: nil).html_safe
   end
 
+  def get_all_leafs extraction_id
+    Instance.where(extraction_id: extraction_id).where(is_leaf: true).order('id ASC')
+  end
+
 end
