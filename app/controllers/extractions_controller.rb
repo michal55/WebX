2fc2d4b554ac6d8bb2f4ccd42a3599999acd4a4d
rastislav krchnavy <rastislav.krchnavy@gmail.com>
2017-04-09 17:25:28 +0200

    authorize off

--- app/controllers/extractions_controller.rb
+++ app/controllers/extractions_controller.rb
@@ -1,5 +1,5 @@
 class ExtractionsController < ApplicationController
-  load_and_authorize_resource :except => [:create, :new]
+
   def index
     @script = Script.find(params[:script_id])
     @extraction = Extraction.where(script_id: @script.id).order('created_at DESC').page params[:page]
