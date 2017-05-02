eb0beebccd7385b2fce1f11b85439f3fc9525ac8
michal55 <michal.kren94@gmail.com>
2017-05-02 03:43:45 +0200

    Add filter postprocessing

--- app/models/instance.rb
+++ app/models/instance.rb
@@ -1,5 +1,6 @@
 class Instance < ActiveRecord::Base
-  has_many :extraction_data
+  has_many :extraction_data, dependent: :destroy
+  has_many :children, foreign_key: "parent_id", class_name: "Instance", dependent: :destroy
   belongs_to :extraction
   acts_as_paranoid
 end
