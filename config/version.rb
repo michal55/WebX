470392977d748e9df421f9e8f5d817678c5bd02d
michal55 <michal.kren94@gmail.com>
2017-05-24 20:02:02 +0200

    "Bump version to 1.4.2.--no-push"

--- config/version.rb
+++ config/version.rb
@@ -2,9 +2,9 @@ module WebX
   module VERSION
     MAJOR = 1
     MINOR = 4
-    PATCH = 1
+    PATCH = 2
 
-    PRE = nil
+    PRE = '--no-push'
 
     STRING = [MAJOR, MINOR, PATCH, PRE].compact * '.'
   end
